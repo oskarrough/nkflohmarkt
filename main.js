6760ba18279e0d542ddb832bd66cf844e774d720
gregory vespasien <vespasiengregory@gmail.com>
2019-04-04 14:50:42 +0200

    change logo and map

---
+++ main.js
@@ -0,0 +1,35 @@
+// listeners on the two booking boxes routing to corresponding checkout pages
+ 36 
+const stripe = Stripe('pk_test_mZkntVL2ik2gfHlTjm3JbHuK006E6ncmTT', {
+	betas: ['checkout_beta_4']
+});
+ 
+const bookingBoxes = document.querySelectorAll('.BookingContainer-box');
+bookingBoxes.forEach(box => {  
+		box.addEventListener('click', function (event) {
+			// When the customer clicks on the button, redirec
+			// them to Checkout.
+			const userChoice = event.target.className.split(' ')[1]; // individual or business box
+			let skuCode = 'sku_EonihanvlPqdvu'; // individual product in dashboard
+			if (userChoice === "BusinessBox") {
+				skuCode = 'sku_EoniSO7y5sqZiX';
+		 	}
+			stripe.redirectToCheckout({
+				items: [{sku: skuCode, quantity: 1}],
+ 
+			 	// Note that it is not guaranteed your customers will be redirected to thi			    // URL *100%* of the time, it's possible that they could e.g. close the
+			 	// tab between form submission and the redirect.
+			 	successUrl: 'https://ead9c19f.ngrok.io/success',
+			 	cancelUrl: 'https://ead9c19f.ngrok.io/canceled',
+			})
+     
+		 	.then(function (result) {
+				if (result.error) {
+					// If `redirectToCheckout` fails due to a browser or networ    k
+					// error, display the localized error message to your custo    mer.
+					const displayError = document.getElementById('error-message    ');
+					displayError.textContent = result.error.message;
+				}
+			});
+		});
+});
